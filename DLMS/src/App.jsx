073a45283fb6a1cfbed6aa1683a04c9dda0023e7
Vedant <113195941+BodkheVedant@users.noreply.github.com>
2024-03-17 15:09:26 +0530

    Done!
    Mini Project created successfully under the guidance of DR. V.P Mahatme

---
+++ DLMS/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import About from "./Components/about/About"
+function App(){
+  return (
+  <>
+  <h1>HEllO World</h1>
+  <About></About>
+  </>
+  );
+}
+
+export default App;
